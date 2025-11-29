c1e63114789869abcc068bb2d88c31e168b68396
KlarkKe <tamnends@gmail.com>
2025-11-29 05:33:52 +0300

    upd UI radius

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+//import { label } from "three/tsl";
 import {Pane} from 'tweakpane';
 
 const pane = new Pane();
@@ -24,7 +25,28 @@ const scene = new THREE.Scene();
 // geometry.setAttribute('position', bufferAttribute)
 
 // objects
-const geometry = new THREE.SphereGeometry(1,16,16);
+let geometry = new THREE.SphereGeometry(1,16,16);
+
+const sphereParameters = {
+  radius: 1,
+  widthSegments: 16,
+  heightSegments: 16
+}
+
+pane
+  .addBinding(sphereParameters, 'radius', {
+  min: 1,
+  max: 20,
+  step: 0.1,
+  label: 'Radius'
+})
+.on('change', (ev)=>{
+  geometry = new THREE.SphereGeometry(ev.value, sphereParameters.widthSegments, sphereParameters.heightSegments);
+  geometryMesh.geometry = geometry;
+})
+
+
+
 const mat1 = new THREE.MeshBasicMaterial({ color: "deeppink", wireframe: true });
 //const mat2 = new THREE.MeshBasicMaterial({ color: "deeppink"});
 
@@ -33,12 +55,19 @@ const geometryMesh = new THREE.Mesh(geometry, mat1);
 
 scene.add(geometryMesh);
 
-pane.addBinding(geometryMesh.scale, 'x', {
-  min: 0,
-  max: 10,
-  step: 0.1
-})
+// pane.addBinding(geometryMesh.scale, 'x', {
+//   min: 0,
+//   max: 10,
+//   step: 0.1,
+//   label: 'Scale X'
+// })
 
+// pane.addBinding(geometryMesh.scale, 'y', {
+//   min: 0,
+//   max: 10,
+//   step: 0.1,
+//   label: 'Scale Y'
+// })
 
 const axel = new THREE.AxesHelper(2);
 scene.add(axel);
